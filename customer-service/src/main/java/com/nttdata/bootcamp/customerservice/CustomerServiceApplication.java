7b1fbb99e31b41bd27b06e589e563fdd261fdd63
Alex B <ales.bejarano@gmail.com>
2022-10-22 10:28:04 -0500

    customer-update-profiles

--- customer-service/src/main/java/com/nttdata/bootcamp/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/nttdata/bootcamp/customerservice/CustomerServiceApplication.java
@@ -2,8 +2,11 @@ package com.nttdata.bootcamp.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
