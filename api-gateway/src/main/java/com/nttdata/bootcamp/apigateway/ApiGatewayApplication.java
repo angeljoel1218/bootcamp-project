303f26aa94436c8f1164d2945632d3f7b325dbd9
Alex B <ales.bejarano@gmail.com>
2022-11-06 19:23:30 -0500

    Add: Check style and sonar

--- api-gateway/src/main/java/com/nttdata/bootcamp/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/nttdata/bootcamp/apigateway/ApiGatewayApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ *
+ * @since 2022
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGatewayApplication.class, args);
+  }
 }
